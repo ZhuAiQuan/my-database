96ae2b0916e7bb65407dfbfecf2f38b36b5af312
zhuaq <zhuaq@7000mall.com>
2022-04-08 09:39:14 +0800

    build vercel test

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ * @Description: xingp，yyds
+ * @Author: zaq
+ * @Date: 2022-04-08 09:38:15
+ * @LastEditTime: 2022-04-08 09:38:16
+ * @LastEditors: zaq
+ * @Reference: 
+ */
+require('./src/main')
