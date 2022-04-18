9693c6590686720a0a394ed385a3d03672f7df85
zhuaq <zhuaq@7000mall.com>
2022-04-18 13:23:19 +0800

    更新电影、剧集、综艺、动漫数据接口

--- src/router/films/index.js
+++ src/router/films/index.js
@@ -2,7 +2,7 @@
  * @Description: xingp，yyds
  * @Author: zaq
  * @Date: 2022-04-15 09:34:45
- * @LastEditTime: 2022-04-15 17:39:19
+ * @LastEditTime: 2022-04-18 13:20:20
  * @LastEditors: zaq
  * @Reference:
  */
@@ -12,6 +12,7 @@ const cheerio = require("cheerio");
 const axios = require("../../utils");
 const { getStrId, parse } = require("../../utils/tools");
 const baseUrl = "https://www.pkmp4.com";
+const urlencode = require("urlencode");
 
 films.get("/", async (ctx) => {
   const { data: html } = await axios.request(baseUrl);
@@ -21,14 +22,14 @@ films.get("/", async (ctx) => {
     const obj = {};
     obj.id = $(v).find("a").attr("href");
     obj.id = getStrId(obj.id);
-    obj.minBg = baseUrl + $(v).find("a img").attr("src");
+    obj.bg = baseUrl + $(v).find("a img").attr("src");
     obj.title = $(v).find("a .txt").text();
     obj.description = $(".foucebox .bd .showDiv")
       .eq(i)
       .find(".foucebox_bg p")
       .text();
-    obj.bigBg =
-      baseUrl + $(".foucebox .bd .showDiv").eq(i).find("a img").attr("src");
+    // obj.bigBg =
+    //   baseUrl + $(".foucebox .bd .showDiv").eq(i).find("a img").attr("src");
     banner.push(obj);
   });
   const films_recommend = [];
@@ -231,60 +232,118 @@ films.get("/play", async (ctx) => {
     link: `https://www.pkmp4.com/addons/dplayer/?url=${url}`,
   };
 });
-films.get('/search', async ctx => {
-  const {wd} = ctx.query;
-  const {data: html} = await axios.request({
+films.get("/search", async (ctx) => {
+  const { wd } = ctx.query;
+  const { data: html } = await axios.request({
     url: `${baseUrl}/vs/-------------.html?wd=${wd}`,
     headers: {
-      referer: baseUrl
-    }
+      referer: baseUrl,
+    },
   });
   const $ = cheerio.load(html);
   const list = [];
-  const all = parseInt($('.main .breadcrumbs').text().substring(4));
-  const temp = $('.main .sr_lists dl');
+  const all = parseInt($(".main .breadcrumbs").text().substring(4));
+  const temp = $(".main .sr_lists dl");
   if (all && temp.length) {
     //
-    
+
     temp.each((i, v) => {
       const obj = {};
-      obj.pic = baseUrl + $(v).find('dt a img').attr('src');
-      const _t = $(v).find('dd p');
-      obj.title = _t.eq(0).find('strong a').text();
-      obj.id = getStrId(_t.eq(0).find('strong a').attr('href'));
-      obj.state = _t.eq(0).find('.ss1').text();
+      obj.pic = baseUrl + $(v).find("dt a img").attr("src");
+      const _t = $(v).find("dd p");
+      obj.title = _t.eq(0).find("strong a").text();
+      obj.id = getStrId(_t.eq(0).find("strong a").attr("href"));
+      obj.state = _t.eq(0).find(".ss1").text();
       obj.nickname = _t.eq(1).text();
       obj.local = _t.eq(2).text();
       obj.starring = _t.eq(3).text();
       obj.time = _t.eq(4).text();
-      obj.description = _t.eq(5).text()
+      obj.description = _t.eq(5).text();
 
-      list.push(obj)
-    })
+      list.push(obj);
+    });
     ctx.body = {
       count: all,
       list,
-      msg: '查询成功'
-    }
+      msg: "查询成功",
+    };
   } else {
     ctx.body = {
       count: all,
       msg: `没有搜到有关于${wd}的信息`,
       list: [],
-      html
-    }
+      html,
+    };
   }
-})
-films.get('/parse', async ctx => {
-  const {url} = ctx.query;
+});
+films.get("/parse", async (ctx) => {
+  const { url } = ctx.query;
   const links = [];
-  for(const key in parse) {
-    links.push(`${parse[key]}${url}`)
+  for (const key in parse) {
+    links.push(`${parse[key]}${url}`);
   }
   ctx.body = {
-    msg: '获取链接成功!',
-    links
+    msg: "获取链接成功!",
+    links,
+  };
+});
+/**
+ * style 影片类型1电影2电视剧3综艺4动漫
+ */
+films.get("/stock", async (ctx) => {
+  let { page, sort, local, type, era, lang, style } = ctx.query;
+  sort = sort || "time";
+  local = local === '全部' || !local ? '' : urlencode(local);
+  type = type === '全部' || !type ? '' : urlencode(type);
+  era = era === '全部' || !era ? '' : urlencode(era);
+  lang = lang === '全部' || !lang ? '' : urlencode(lang);
+  page = page || "1";
+  style = style || '1';
+  const { data: html } = await axios.request(
+    `https://www.pkmp4.com/ms/${style}-${local}-${sort}-${type}-${lang}----${page}---${era}.html`
+  );
+
+  const $ = cheerio.load(html);
+  const tag = {
+      type: [],
+      era: [],
+      local: [],
+      lang: [],
+      sort: ["time", "hits", "score"],
+    },
+    list = [];
+  for (let i = 1; i < 5; i++) {
+    $(`.list-box .c${i} a`).each((j, v) => {
+      if (i === 1) tag.type.push($(v).text());
+      else if (i === 2) tag.era.push($(v).text());
+      else if (i === 3) tag.local.push($(v).text());
+      else if (i === 4) tag.lang.push($(v).text());
+    });
   }
-})
+  if ($(".content-list li").length) {
+    $(".content-list li").each((i, v) => {
+      const obj = {};
+      obj.id = getStrId($(v).find(".li-img a").attr("href"));
+      obj.title = $(v).find(".li-img a").attr("title");
+      obj.playCount = $(v).find(".li-img a .bottom .bottom1").text();
+      obj.filmQuality = $(v).find(".li-img a .bottom .bottom2 ").text();
+      obj.grade = $(v).find(".li-bottom h3 span").text();
+      obj.tag = $(v).find(".li-bottom .tag").text();
+      list.push(obj);
+    });
+  }
+  if (list.length) {
+    ctx.body = {
+      list,
+      tag,
+    };
+  } else {
+    ctx.body = {
+      tag,
+      list,
+      msg: "暂无此类型影片",
+    };
+  }
+});
 
 module.exports = films;
